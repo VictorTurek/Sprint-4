f93cb2b5fb78c55a43c355d03533ee642f8a71df
VictorTurek <victorturekbcn@gmail.com>
2023-12-20 12:15:50 +0100

    ejercicio 1 terminado

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,27 @@
 "use strict";
-let test;
-let test2;
+const apiUrl = "https://icanhazdadjoke.com/";
+const requestOptions = {
+    method: "GET",
+    headers: {
+        "Accept": "application/json", // Especifica el formato de respuesta que deseas (JSON en este caso)
+    },
+};
+document.addEventListener("DOMContentLoaded", () => {
+    //console.log("La página se ha cargado completamente.");
+    randomJoke();
+});
+function randomJoke() {
+    fetch(apiUrl, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+        const joke = data.joke;
+        let randomJokes = document.getElementById("randomJokes");
+        randomJokes.innerText = joke;
+        //console.log(joke);
+    })
+        .catch(error => {
+        console.error("Error al obtener el chiste:", error);
+        let randomJokes = document.getElementById("randomJokes");
+        randomJokes.innerText = `Error al cargar el chiste. Detalles: ${error.message}`;
+    });
+}
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,6 +1,30 @@
 
+const apiUrl: string = "https://icanhazdadjoke.com/";
 
+const requestOptions = {
+    method: "GET",
+    headers: {
+        "Accept": "application/json", // Especifica el formato de respuesta que deseas (JSON en este caso)
+    },
+};
 
-function nextJoke(){
+document.addEventListener("DOMContentLoaded", () => { // Al cargarse completamente la pagina, se ejecuta la funcion randomJoke()
+    //console.log("La página se ha cargado completamente.");
+    randomJoke();
+});
 
+function randomJoke() {
+    fetch(apiUrl, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            const joke = data.joke;
+            let randomJokes: HTMLElement | null = document.getElementById("randomJokes")!;
+            randomJokes.innerText = joke;
+            //console.log(joke);
+        })
+        .catch(error => {
+            console.error("Error al obtener el chiste:", error); 
+            let randomJokes: HTMLElement | null = document.getElementById("randomJokes")!;
+            randomJokes.innerText = `Error al cargar el chiste. Detalles: ${error.message}`;
+        });
 }
